d2cbd6dfc8cffbd91b018cd3d0427f494709bb18
unknown <cclayt02@CSPC104.dcs.bbk.ac.uk>
2014-11-15 08:12:02 +0000

    implemented method to test with, added first simple test

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -1,3 +1,19 @@
 public class FractionCalculatorTest{
+	
+	FractionCalculator calc = new FractionCalculator();
 
+	public static void main(String[] args) {
+		FractionCalculatorTest test = new FractionCalculatorTest();
+		test.testExpressionString("1/3 + 1/3", new Fraction(2,3), "Test 01: Simple addition");
+	}
+	
+	
+	/* tests expressions through a FractionCalculator's evaluate method */
+	private void testExpressionString(String stringInput, Fraction expectedAnswer, String testId ){
+		Fraction answer = calc.evaluate(new Fraction(0), stringInput);
+		if (! answer.equals(expectedAnswer)){
+			System.out.println(testId + " FAILED: 1/2 + 1/3 evaluates to: " + answer.toString() + ". Expected answer: " + expectedAnswer.toString());  	
+		}
+	}
+	
 }
