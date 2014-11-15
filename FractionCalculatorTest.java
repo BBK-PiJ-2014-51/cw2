7abdb3b34bed431d6a818421db573accc1d6111d
unknown <cclayt02@CSPC102.dcs.bbk.ac.uk>
2014-11-15 18:52:13 +0000

    added failing tests for functons (abs value, negate, clear)

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -4,7 +4,17 @@ public class FractionCalculatorTest{
 
 	public static void main(String[] args) {
 		FractionCalculatorTest test = new FractionCalculatorTest();
+		//basic two operand tests
 		test.testExpressionString("1/3 + 1/3", new Fraction(2,3), "Test 01: Simple addition");
+		test.testExpressionString("1/2 - 1/4", new Fraction(1,4), "Test 02: Simple subtraction");
+		test.testExpressionString("1/2 * 1/2", new Fraction(1,4), "Test 03: Simple multiplication");
+		test.testExpressionString("1/4 / 1/3", new Fraction(3,4), "Test 04: Simple division");
+		//basic tests with commands
+		test.testExpressionString("1/3 - 2/3 N", new Fraction(1,3), "Test 05: Negative");
+		test.testExpressionString("1/2 - 1/4 c", new Fraction(0,1), "Test 06: Clear");
+		test.testExpressionString("1/2 * -1/2 abs", new Fraction(1,4), "Test 07: Absolute Value");
+		//test.testExpressionString("1/4 / 1/3 q", null, "Test 08: Quit"); //test quit?
+	
 	}
 	
 	/* tests expressions through a FractionCalculator's evaluate method */
