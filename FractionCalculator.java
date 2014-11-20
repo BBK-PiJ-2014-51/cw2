4cc42d475b2ad039df68b8e0ec067174d83515e2
unknown <cclayt02@CSPC019.dcs.bbk.ac.uk>
2014-11-20 19:46:29 +0000

    added comments, put methods in a more logical order

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,7 +1,6 @@
 public class FractionCalculator{
 	private Fraction currentValue = new Fraction(0);
 	private String currentOperator = null;
-
 	
 	public static void main(String[] args) {
 		FractionCalculator calc = new FractionCalculator();
@@ -10,39 +9,6 @@ public class FractionCalculator{
 		calc.quit();
 	}
 	
-	private void loop() {
-		boolean validInput = true;
-		while (validInput){
-			 String inputString = System.console().readLine();
-			if (isValidInput(inputString)){
-				evaluate(this.currentValue, inputString); 
-			} else{
-				break;
-			}
-		}
-	}
-
-	private boolean isValidInput(String inputString) {
-		String[] tokens = inputString.split("\\s");	
-		for (int i = 0; i < tokens.length; i++){
-			if (!isValidToken(tokens[i])) return false;
-		}
-		return true;
-	}
-	
-	private boolean isValidToken(String token){
-		return (isFraction(token) || isOperator(token) || isCommand(token));
-	}
-
-	private void printWelcomeMsg() {
-		System.out.println("Welcome to my fraction calculator for PiJ coursework 2.");
-		System.out.println("By Caleb Clayton.");
-	}
-	
-	private void quit() {
-		// TODO Implement	
-	}
-
 	public Fraction evaluate(Fraction fraction, String inputString) {
 		//if (!inputString.contains("\\s"));
 		inputString = inputString.toLowerCase();
@@ -62,72 +28,41 @@ public class FractionCalculator{
 		return this.currentValue;
 	}
 	
-	private void applyNextFraction(String token){
-		Fraction nextFraction = this.parseFraction(token);
-		if (this.currentOperator != null){ // if an operator is stored in memory already, we should calculate
-			this.currentValue = this.calculate(nextFraction);
-			this.currentOperator = null; //reset operator to null
-		} else { // if instead no operator is stored in memory, set current value to the parsed fraction
-			this.currentValue = nextFraction;
+	private void loop() {
+		boolean validInput = true;
+		while (validInput){
+			 String inputString = System.console().readLine();
+			if (isValidInput(inputString)){
+				evaluate(this.currentValue, inputString); 
+			} else {
+				break;
+			}
 		}
 	}
 	
-		
-	private void executeCommand(String token) {
-		if (token.equals("a") || token.equals("abs")){
-			this.currentValue = currentValue.absValue();
-		} else if (token.equals("n")  || token.equals("neg")){
-			this.currentValue = currentValue.negate();
-		} else if (token.equals("c") || token.equals("clear")){
-			this.reset();
-		} else if (token.equals("q") || token.equals("quit")){
-			this.quit(); //probably not what im going to do ultimately. 
-		} else {
-			
-		}
+	private void printWelcomeMsg() {
+		System.out.println("Welcome to my fraction calculator for PiJ coursework 2.");
+		System.out.println("By Caleb Clayton.");
 	}
-
+	
+	private void quit() {
+		// TODO Implement	
+	}
+	
+	/**
+	 * Resets the calculator to initial state.
+	 */
 	private void reset() {
 		this.currentValue = new Fraction(0);
 		this.currentOperator = null;
 	}
 
-	private boolean isFraction(String token) {
-		String[] fractionComponents = token.split("/");
-		if (fractionComponents.length > 2) return false;
-		//check that the fraction components contain only digits, with the exception of the first character,
-		//which may be a negative sign
-		for (int i = 0; i < fractionComponents.length; i++){
-			if (fractionComponents[i].charAt(0) == '-') fractionComponents[i] = fractionComponents[i].substring(1);
-			for(int j = 0; j < fractionComponents[i].length(); j++){
-				if(!Character.isDigit(fractionComponents[i].charAt(j)))return false;
-			}
-		}
-		return true;
-	}
-	
-	private boolean isCommand(String token) {
-		if (token.equals("a") || token.equals("abs") || token.equals("n") || token.equals("neg")
-			|| token.equals("c") || token.equals("clear") || token.equals("q") || token.equals("quit")){
-			return true;
-		} else {
-			return false;
-		}		
-	}
-	
-	private boolean isOperator(String token) {
-		if(token.length() != 1) return false; //operators are always a single char
-		switch(token.charAt(0)){
-			case '+':
-			case '-':
-			case '*':
-			case '/':
-				return true;
-			default:
-				return false;
-		}
-	}
-
+	/**
+	 * Performs arithmetic operation based on the value current in memory,
+	 * the operator currently in memory, and the given Fraction.
+	 * @param nextFraction
+	 * @return
+	 */
 	private Fraction calculate(Fraction nextFraction) {
 		if(this.currentOperator.equals("+")){
 			return currentValue.add(nextFraction);
@@ -142,7 +77,48 @@ public class FractionCalculator{
 			return null;
 		}
 	}
-
+	
+	/**
+	 * Applies a fraction, given as a string, to the calculator.
+	 * If an operator is in memory a calculation is performed via calculate(),
+	 * the fraction returned from calculate is then stored in memory.
+	 * If no operator is in memory then the given fraction is stored.
+	 * @param token
+	 */
+	private void applyNextFraction(String token){
+		Fraction nextFraction = this.parseFraction(token);
+		if (this.currentOperator != null){ // if an operator is stored in memory already, we should calculate
+			this.currentValue = this.calculate(nextFraction);
+			this.currentOperator = null; //reset operator to null
+		} else { // if instead no operator is stored in memory, set current value to the parsed fraction
+			this.currentValue = nextFraction;
+		}
+	}
+	
+	/**
+	 * Executes the appropriate command, which is given as a string, on the value stored in memory	
+	 * @param token
+	 */
+	private void executeCommand(String token) {
+		if (token.equals("a") || token.equals("abs")){
+			this.currentValue = currentValue.absValue();
+		} else if (token.equals("n")  || token.equals("neg")){
+			this.currentValue = currentValue.negate();
+		} else if (token.equals("c") || token.equals("clear")){
+			this.reset();
+		} else if (token.equals("q") || token.equals("quit")){
+			this.quit(); //probably not what im going to do ultimately. 
+		} else {
+			
+		}
+	}
+	
+	/**
+	 * Splits a fraction passed as a string into a numerator and denominator
+	 * and then instantiates a new Fraction object. 
+	 * @param token
+	 * @return
+	 */
 	private Fraction parseFraction(String token){
 		if (token.contains("/")) {
 			String[] fractionComponents = token.split("/");
@@ -152,4 +128,82 @@ public class FractionCalculator{
 		}
 	}
 
+	/**
+	 * Validates a fraction given as a string.
+	 * Fractions are valid if they are non-empty strings, containing no more than two tokens that
+	 * are delimited with a '/' and the tokens contain only numbers, with the exception of the first
+	 * character which may indicate a negative number.
+	 * @param token
+	 * @return
+	 */
+	private boolean isFraction(String token) {
+		if (token.length() == 0) return false;
+		String[] fractionComponents = token.split("/");
+		if (fractionComponents.length > 2) return false;
+		//check that the fraction components contain only digits, with the exception of the first character,
+		//which may be a negative sign
+		for (int i = 0; i < fractionComponents.length; i++){
+			if (fractionComponents[i].charAt(0) == '-') fractionComponents[i] = fractionComponents[i].substring(1);
+			for(int j = 0; j < fractionComponents[i].length(); j++){
+				if(!Character.isDigit(fractionComponents[i].charAt(j))) return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Validates command by comparing the given string against a list of valid commands.
+	 * @param token
+	 * @return
+	 */
+	private boolean isCommand(String token) {
+		if (token.equals("a") || token.equals("abs") || token.equals("n") || token.equals("neg")
+			|| token.equals("c") || token.equals("clear") || token.equals("q") || token.equals("quit")){
+			return true;
+		} else {
+			return false;
+		}		
+	}
+	
+	/**
+	 * Validates operator by comparing the given string against a list of valid commands.
+	 * @param token
+	 * @return
+	 */
+	private boolean isOperator(String token) {
+		if(token.length() != 1) return false; //operators are always a single char
+		switch(token.charAt(0)){
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	/**
+	 * Checks all tokens in a given input string for validity.
+	 * A string is valid if all its tokens are valid.
+	 * @param inputString
+	 * @return boolean
+	 */
+	private boolean isValidInput(String inputString) {
+		String[] tokens = inputString.split("\\s");	
+		for (int i = 0; i < tokens.length; i++){
+			if (!isValidToken(tokens[i])) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Checks a given token for validity.
+	 * A token is valid if it is recognized as an operator, command, or fraction. 
+	 * @param token
+	 * @return
+	 */
+	private boolean isValidToken(String token){
+		return (isFraction(token) || isOperator(token) || isCommand(token));
+	}
 }
